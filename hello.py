fec2c709a72b1449bafbf51d235a176ef463c6ea
RhettHarry43us <harry43us@yahoo.com>
2025-09-27 17:17:10 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello.py
+# First program for The Day the Clouds Opened repo
+# Author: Harry G Wright
+
+def main():
+    print("Hello, World! The Day the Clouds Opened Project has begun!")
+
+if __name__ == "__main__":
+    main()
